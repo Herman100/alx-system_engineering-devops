85258c9714ddaefc624b073b9dbfa3620b17c797
Herman100 <hermankwamebour30@gmail.com>
2023-07-04 16:55:59 +0000

    textme regexp

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-File.foreach('text_messages.log') do |line|
-  match = line.match(/\[from:(?<sender>\S+)\] \[to:(?<receiver>\S+)\] \[flags:(?<flags>\S+)\]/)
-  puts "#{match[:sender]},#{match[:receiver]},#{match[:flags]}" if match
-end
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
