77049c4caf01019c761ea8fbe132ad268aa7bd67
Herman100 <hermankwamebour30@gmail.com>
2023-08-25 15:02:21 +0000

    exec of puppet

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# killing a process killmenow
+
+exec { 'kill_killmenow_process':
+  command => 'pkill killmenow',
+  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+  onlyif  => 'pgrep killmenow',
+  provider => shell,
+}
