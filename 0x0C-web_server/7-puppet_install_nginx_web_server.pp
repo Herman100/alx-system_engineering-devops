a07c94583dcc20328d5923470e59fbbdf5bb8ecd
Herman100 <hermankwamebour30@gmail.com>
2023-08-30 02:35:51 +0000

    puppet config

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,41 @@
+# nginx package installation w puppet
+
+# This class installs and configures an Nginx server
+class nginx {
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  file { '/var/www/html/index.nginx-debian.html':
+    ensure  => file,
+    content => 'Hello World!',
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    server_name _;
+    location / {
+        try_files $uri $uri/ =404;
+    }
+    location = /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}',
+    require => Package['nginx'],
+  }
+
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/nginx/sites-available/default'],
+  }
+}
+
+include nginx
