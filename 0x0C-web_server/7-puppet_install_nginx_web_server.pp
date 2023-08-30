6b9d3361751b365a7a783a3f82e1dae70151b0e1
Herman100 <hermankwamebour30@gmail.com>
2023-08-30 03:07:07 +0000

    pp update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,34 @@
-# Automates installation and configuration of nginx
+# nginx package installation w puppet
 
 package { 'nginx':
   ensure => installed,
 }
 
 file { '/var/www/html/index.nginx-debian.html':
-  ensure  => present,
+  ensure  => file,
   content => 'Hello World!',
 }
 
 file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content =>
-"server {
-		listen 80 default_server;
-
-        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location / {
-                try_files \$uri \$uri/ =404;
-        }
-}"
+  ensure  => file,
+  content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    server_name _;
+    location / {
+        try_files $uri $uri/ =404;
+    }
+    location = /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}',
 }
 
 service { 'nginx':
   ensure    => running,
   enable    => true,
-  hasstatus => true,
-  require   => Package['nginx'],
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
