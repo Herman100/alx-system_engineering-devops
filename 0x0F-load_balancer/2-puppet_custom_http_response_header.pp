10b77b9bb4a5cdfe7f5f4f29a0470b6d2a25ad53
Herman100 <hermankwamebour30@gmail.com>
2023-09-04 23:21:03 +0000

    line not test

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,9 +4,11 @@ package { 'nginx':
 }
 
 # Set the hostname as the value for the custom HTTP header "X-Served-By"
-exec { 'nginx_custom_header':
-  command => "sed -i '16i\add_header X-Served-By ${hostname};' /etc/nginx/nginx.conf",
-  unless  => "grep '^add_header X-Served-By' /etc/nginx/nginx.conf",
+file_line { 'custom_header_line':
+  ensure => present,
+  path   => '/etc/nginx/nginx.conf',
+  line   => "add_header X-Served-By ${hostname};",
+  match  => '^add_header X-Served-By',
 }
 
 # Restart Nginx to apply changes
@@ -14,5 +16,5 @@ service { 'nginx':
   ensure     => running,
   enable     => true,
   hasrestart => true,
-  subscribe  => Exec['nginx_custom_header'],
+  subscribe  => File_line['custom_header_line'],
 }
