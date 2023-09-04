bb87d563875005288fa0eea9372a6883a30701de
Herman100 <hermankwamebour30@gmail.com>
2023-09-04 23:08:58 +0000

    puppet code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,18 @@
-#puppet advance
-exec { 'update':
-  command  => 'sudo apt-get update',
-  provider => shell,
+# Install Nginx
+package { 'nginx':
+  ensure => installed,
 }
--> package {'nginx':
-  ensure => present,
+
+# Set the hostname as the value for the custom HTTP header "X-Served-By"
+exec { 'nginx_custom_header':
+  command => "sed -i '16i\add_header X-Served-By ${hostname};' /etc/nginx/nginx.conf",
+  unless  => "grep '^add_header X-Served-By' /etc/nginx/nginx.conf",
 }
--> file_line { 'header line':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "	location / {
-  add_header X-Served-By ${hostname};",
-  match  => '^\tlocation / {',
-}
--> exec { 'restart service':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+
+# Restart Nginx to apply changes
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  subscribe  => Exec['nginx_custom_header'],
 }
