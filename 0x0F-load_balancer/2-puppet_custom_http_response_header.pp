4cf75bed302b7dd3b6e5826c023d3fcc5529f481
Herman100 <hermankwamebour30@gmail.com>
2023-09-04 21:50:50 +0000

    code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,3 +1,8 @@
+# Install Nginx
+package { 'nginx':
+  ensure => present,
+}
+
 file { '/etc/nginx/nginx.conf':
   content => template('custom_http_response_header.conf.erb'),
 }
@@ -7,4 +12,11 @@ template 'custom_http_response_header.conf.erb' {
   variables => {
     hostname => $::hostname,
   },
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  hasstatus => true,
+  require   => Package['nginx'],
 }
