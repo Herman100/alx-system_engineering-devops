cdc68898b81c90102362bf33d819fe4980d542ac
Herman100 <hermankwamebour30@gmail.com>
2023-10-10 13:05:16 +0000

    top ten hot post subreeddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,13 +5,13 @@ import requests
 
 def top_ten(subreddit):
     """
-    Queries the Reddit API and prints the titles of 
+    Queries the Reddit API and prints the titles of
     the first 10 hot posts listed for a given subreddit.
     If an invalid subreddit is given, the function prints None.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'popular_api_hot'}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
@@ -19,6 +19,6 @@ def top_ten(subreddit):
         return
 
     data = response.json().get('data', {}).get('children', [])
- 
+
     for post in data:
         print(post.get('data', {}).get('title'))
